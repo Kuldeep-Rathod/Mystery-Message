290807dd7f1885e6ccef4656a617be5a7d6bfbc2
Kuldeep Rathod <kuldiprathod375@gmail.com>
2025-04-03 15:16:25 +0530

    Created page: Verify OTP

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,123 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSlot,
+} from '@/components/ui/input-otp';
+import { verifySchema } from '@/schemas/verifySchema';
+import { ApiResponse } from '@/types/apiResponse';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { Loader } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import { z } from 'zod';
+
+const VerifyCodePage = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const router = useRouter();
+    const params = useParams<{ username: string }>();
+
+    //zod implementation
+    const form = useForm<z.infer<typeof verifySchema>>({
+        resolver: zodResolver(verifySchema),
+    });
+
+    const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+        setIsSubmitting(true);
+        try {
+            const response = await axios.post<ApiResponse>('/api/verify-code', {
+                username: params.username,
+                code: data.code,
+            });
+            toast.success(response.data.message);
+            router.replace(`/sign-in`);
+        } catch (error) {
+            console.error('Error in signup user', error);
+
+            const axiosError = error as AxiosError<ApiResponse>;
+            const errorMessage = axiosError.response?.data.message;
+            toast.error('SignUp Failed', {
+                description: errorMessage,
+            });
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    return (
+        <div className='flex justify-center items-center min-h-screen bg-gray-800'>
+            <div className='w-full max-w-md flex flex-col justify-center items-center p-8 space-y-8 bg-white rounded-lg shadow-md'>
+                <div className='text-center'>
+                    <h1 className='text-3xl font-extrabold tracking-tight lg:text-4xl mb-6'>
+                        Verify Your Account
+                    </h1>
+                    <p className='mb-4'>
+                        Enter the verification code sent to your email
+                    </p>
+                </div>
+
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className='w-2/3 space-y-6 flex flex-col items-center'
+                    >
+                        <FormField
+                            control={form.control}
+                            name='code'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>One-Time Password</FormLabel>
+                                    <FormControl>
+                                        <InputOTP
+                                            maxLength={6}
+                                            {...field}
+                                        >
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={0} />
+                                                <InputOTPSlot index={1} />
+                                                <InputOTPSlot index={2} />
+                                                <InputOTPSlot index={3} />
+                                                <InputOTPSlot index={4} />
+                                                <InputOTPSlot index={5} />
+                                            </InputOTPGroup>
+                                        </InputOTP>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <Button
+                            type='submit'
+                            disabled={isSubmitting}
+                            className='flex mx-auto'
+                        >
+                            {isSubmitting ? (
+                                <>
+                                    <Loader className='mx-4 animate-spin' />
+                                </>
+                            ) : (
+                                'Submit'
+                            )}
+                        </Button>
+                    </form>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default VerifyCodePage;
