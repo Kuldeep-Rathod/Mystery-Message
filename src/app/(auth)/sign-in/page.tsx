84acc7a5cd1c9d06e5416f995c53f8ad1f2ad201
Kuldeep Rathod <kuldiprathod375@gmail.com>
2025-04-03 16:10:02 +0530

    Created page: Sign in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,146 @@
-import React from 'react'
+'use client';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Loader } from 'lucide-react';
+import Link from 'next/link';
+import { signInSchema } from '@/schemas/signInSchema';
+import { signIn } from 'next-auth/react';
 
-export default page
+const SignInPage = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const router = useRouter();
+
+    //zod implementation
+    const form = useForm<z.infer<typeof signInSchema>>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            identifier: '',
+            password: '',
+        },
+    });
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+        setIsSubmitting(true);
+        try {
+            const response = await signIn('credentials', {
+                redirect: false,
+                identifier: data.identifier,
+                password: data.password,
+            });
+
+            if (response?.error) {
+                toast.error(response.error);
+                setIsSubmitting(false);
+                return;
+            }
+
+            if (response?.ok) {
+                toast.success('Sign in successful');
+                router.replace('/');
+            }
+        } catch (error) {
+            console.error('Sign in error:', error);
+            toast.error('An unexpected error occurred');
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    return (
+        <div className='flex justify-center p-4 items-center min-h-screen bg-gray-800'>
+            <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md'>
+                <div className='text-center'>
+                    <h1 className='text-3xl font-extrabold tracking-tight lg:text-4xl mb-6'>
+                        Welcome back True Feedback
+                    </h1>
+                    <p className='mb-4'>
+                        Sign in to start your anonymous adventure
+                    </p>
+                </div>
+
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className='space-y-6'
+                    >
+                        <FormField
+                            name='identifier'
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username/Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder='Username/Email'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name='password'
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type='password'
+                                            placeholder='Password'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button
+                            type='submit'
+                            disabled={isSubmitting}
+                            className='flex mx-auto'
+                        >
+                            {isSubmitting ? (
+                                <>
+                                    <Loader className='mx-4 animate-spin' />
+                                </>
+                            ) : (
+                                'Submit'
+                            )}
+                        </Button>
+                    </form>
+                </Form>
+                <div className='text-center mt-4'>
+                    <p>
+                        You don&apos;t have an account?{' '}
+                        <Link
+                            href='/sign-up'
+                            className='text-blue-600 hover:text-blue-800'
+                        >
+                            Sign Up
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignInPage;
